4ed49a741e9b1e828f12bbd22618d99621fb00d5
Vadym Myroniuk <vadymttt@gmail.com>
2021-08-11 13:31:30 +0200

    added productCollection sidebar

--- Smartmage/Adminmenu/Block/ImageConf.php
+++ Smartmage/Adminmenu/Block/ImageConf.php
@@ -63,14 +63,48 @@ class ImageConf extends \Magento\Framework\View\Element\Template
     public function getProductCollection()
     {
         $collection = $this->collectionFactory->create();
-        $collection->addAttributeToSelect('*');
 
         $quantity_product =     $this->helper->getConfigValue('smartmage_section/general_1/quantity_product');
+        $limit =                $this->helper->getConfigValue('smartmage_section/general_1/quantity_limit');
         $price =                $this->helper->getConfigValue('smartmage_section/general_1/price');
 
+        // echo "quantity_product". $quantity_product. "limit". $limit."price". $price;
+
+        $collection->addAttributeToSelect('*');
+        $collection->joinField(
+            'qty', 'cataloginventory_stock_item', 'qty', 'product_id=entity_id', '{{table}}.stock_id=1', 'left'
+            );
+        $collection->addAttributeToFilter('price', ['lt' => $price]);
+        $collection->addAttributeToFilter('status', ['eq' => 1]);
+        $collection->addAttributeToFilter('visibility', ['in'=> [2,3,4] ] );
+        $collection->addAttributeToFilter('qty', ['gt'=> $quantity_product] );
+
+        $collection->setPageSize($limit );
+
+
+        
         foreach ($collection as $product) {
-            $productIds[] = $product->getId();
+            // print_r($product->getData());
+            // echo "<img title=\"".$imageData["image_title"]."\" src=\"".$imgurl."\" alt=\"".$imageData["image_alt_text"]."\"/>";
+            
+            // echo Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA) . 'catalog/product' . $product->getImage();
+
+            $objectManager =\Magento\Framework\App\ObjectManager::getInstance();
+            $helperImport = $objectManager->get('\Magento\Catalog\Helper\Image');
+            
+            $imageUrl = $helperImport->init($product, 'product_page_image_small')
+                            ->setImageFile($product->getSmallImage()) // image,small_image,thumbnail
+                            ->resize(380)
+                            ->getUrl();
+            // echo $imageUrl;
+
+            echo "<img title=".$product->getData('name')." src=\"".$imageUrl."\"/>";
+            echo "<p> Product Name: " . $product->getData('name') . "</p>";
+            echo "<p> Product Description: " . $product->getDescription() . "</p>";
+            
         }
+
+        return $collection;
 
     }
 }
